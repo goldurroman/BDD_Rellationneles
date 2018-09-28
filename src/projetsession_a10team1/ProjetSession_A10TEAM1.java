2dc84c84ab9f06b21236b9e305e99295f8b256b5
1895066 <1895066@LI-S030-0T20>
2018-09-28 16:31:21 -0400

    Creation du projet avec les insertion efectuees

---
+++ src/projetsession_a10team1/ProjetSession_A10TEAM1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetsession_a10team1;
+
+/**
+ *
+ * @author 1895066
+ */
+public class ProjetSession_A10TEAM1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
